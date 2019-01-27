6957deae56f956bca46d7439735ee77b820eafc3
Anthony Langford <anthony.langford@gmail.com>
2019-01-27 00:05:56 -0500

    🐐🎉goat party🎉🐐

---
+++ src/cumpoenantz/Flash.js
@@ -0,0 +1,65 @@
+export default function Flash() {
+
+  // setup
+
+  let canvas = document.querySelector(`canvas`)
+
+  function handleResize () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  }
+
+  window.onresize = handleResize
+
+  handleResize()
+
+  let mouse = { x: 0, y: 0 }
+
+  window.onmouseover = event =>
+    mouse = { x: event.clientX, y: event.clientY }
+
+  window.onmousemove = event =>
+    mouse = { x: event.clientX, y: event.clientY }
+
+  // the fun part
+
+  let context = canvas.getContext(`2d`)
+
+  function render (time) {
+    time = time % 100000
+
+    canvas.width = canvas.width
+
+    let originX = canvas.width / 2
+    let originY = canvas.height / 2
+
+    let dots = mouse.x
+
+    for (let i = 0; i < dots; i += 1) {
+      let coordinates = [
+        originX + Math.sin(i) * i,
+        originY + Math.cos(i) * i
+      ]
+
+      let radius =
+        Math.max(0, Math.sin(time * i / 30000)) * i / 10
+
+      let angle = [ 0, Math.PI * 2 ]
+
+      let hue = time / i * mouse.y / 150 % 360
+
+      context.beginPath()
+      context.arc(
+        ...coordinates,
+        radius,
+        ...angle
+      )
+      context.fillStyle = `hsl(${ hue }, 67%, 51%)`
+      context.fill()
+    }
+
+    window.requestAnimationFrame(render)
+  }
+
+  render()
+}
---
+++ src/cumpoenantz/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import '../App.css';
+import Flash from './Flash';
+
+export default function Home() {
+  useEffect(() => {
+    Flash();
+  });
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <canvas></canvas>
+        John's a dink
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
