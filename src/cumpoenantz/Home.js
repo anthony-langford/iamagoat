5e6b592a0663635fc2a786c068b53f271a979ff9
Anthony Langford <anthony.langford@gmail.com>
2019-09-10 14:43:38 -0400

    background

--- src/cumpoenantz/Home.js
+++ src/cumpoenantz/Home.js
@@ -1,5 +1,20 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
+
+// Components
 import PartyTime from './PartyTime'
+
+const App = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 const Home = () => {
   useEffect(() => {
@@ -7,11 +22,9 @@ const Home = () => {
   })
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <canvas></canvas>
-      </header>
-    </div>
+    <App>
+      <canvas />
+    </App>
   )
 }
 
